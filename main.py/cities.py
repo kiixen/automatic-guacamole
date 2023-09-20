976d7a715cd107b64768804ebfc4ffc8350a39bd
kiixen <145470159+kiixen@users.noreply.github.com>
2023-09-20 11:45:57 +0000

    kiixen.hometask

---
+++ main.py/cities.py
@@ -0,0 +1,14 @@
+cities = ["Japan","Korea","Canada","China","United States","Austria"]
+print(cities)
+print(sorted(cities))
+print(cities)
+cities.sort(reverse=True)
+print(cities)
+cities.reverse()
+print(cities)
+cities.sort()
+print(cities)
+cities = ["Japan","Korea","Canada","China","United States","Austria"]
+print(cities)
+cities.reverse()
+print(cities)
